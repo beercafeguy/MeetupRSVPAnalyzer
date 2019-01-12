f379767d5b5f4fd6826e038544739027f0c7864b
Hem Chandra <hemchandra@outlook.com>
2019-01-12 15:33:03 +0530

    added kafka binder config and started mongo

---
+++ mongo-start.md
@@ -0,0 +1,14 @@
+"bin/mongod.exe" --dbpath data
+
+rem start the shell
+.\bin\mongo.exe
+
+# RDMBS								MONGO
+# databases 						databases
+# tables							collections
+# row								document
+# column							field
+# primary key					primary key
+# Mongo stores in JSON
+# join 							embedded document
+ 
--- rsvp-collection-app/src/main/java/com/github/beercafeguy/rsvpcollectionapp/RsvpsWebSocketHandler.java
+++ rsvp-collection-app/src/main/java/com/github/beercafeguy/rsvpcollectionapp/RsvpsWebSocketHandler.java
@@ -1,15 +1,28 @@
 package com.github.beercafeguy.rsvpcollectionapp;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import org.springframework.stereotype.Component;
-import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.AbstractWebSocketHandler;
 
 @Component
 public class RsvpsWebSocketHandler extends AbstractWebSocketHandler {
+	
+	private static final Logger logger = 
+            Logger.getLogger(RsvpsWebSocketHandler.class.getName());
+	
+	private final RsvpsKafkaProducer rsvpsKafkaProducer;
+	
+	public RsvpsWebSocketHandler(RsvpsKafkaProducer rsvpsKafkaProducer) {    
+        this.rsvpsKafkaProducer = rsvpsKafkaProducer;
+    }
+	
     @Override
     public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
-        System.out.println(message.getPayload());
+    	logger.log(Level.INFO, "RSVP Message:\n {0}", message.getPayload());
+    	rsvpsKafkaProducer.sendRsvpMessage(message);     
     }
 }
